8ea4f26b5fbec3bf9c1eaa6f0e4ff32e07d7cb12
zinmazin <116417051+zinmazin@users.noreply.github.com>
2022-11-05 21:37:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const app = require("express")();
-
+const express = require('express');
+const path = require('path');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("", (req, res) => {
-    res.send("Hello world");
+app.use(express.static(__dirname));
+
+app.use('/', function (req, res, next) {
+
+    var options = {
+        root: path.join(__dirname)
+    };
+    var fileName = 'Trip-Tool.html';
+    res.sendFile(fileName, options, function (err) { 
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 });
 
-app.listen(PORT, () => {
-    console.log(`App up at port ${PORT}`);
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
+
+app.get('/', function (req, res) {
+    console.log("File Sent")
+    res.send();
 });
